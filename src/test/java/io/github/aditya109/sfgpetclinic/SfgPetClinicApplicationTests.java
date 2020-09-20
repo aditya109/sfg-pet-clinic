ef3e27be5dc9d730b0d597953337cba7c92b7d55
aditya109 <adikid1996@gmail.com>
2020-09-20 19:10:32 +0530

    initializing the project
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ src/test/java/io/github/aditya109/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aditya109.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
