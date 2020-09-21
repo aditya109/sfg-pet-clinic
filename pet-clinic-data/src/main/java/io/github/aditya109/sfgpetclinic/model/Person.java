8d45eff0bea719b690cd3ce5f57d9733967369fc
aditya109 <adikid1996@gmail.com>
2020-09-21 19:54:36 +0530

    adding base entity; closes #10
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- pet-clinic-data/src/main/java/io/github/aditya109/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/io/github/aditya109/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package io.github.aditya109.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/io/github/aditya109/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/io/github/aditya109/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package io.github.aditya109.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
