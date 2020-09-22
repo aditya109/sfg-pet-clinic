456d8d41a2c6ad579eb0e4847a892198547d90e6
aditya109 <adikid1996@gmail.com>
2020-09-22 21:16:44 +0530

    added Pet Clinic Index Page and Controller; closes #8 Signed-off-by: aditya109 <adikid1996@gmail.com>
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ pet-clinic-web/src/main/java/io/github/aditya109/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package io.github.aditya109.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
