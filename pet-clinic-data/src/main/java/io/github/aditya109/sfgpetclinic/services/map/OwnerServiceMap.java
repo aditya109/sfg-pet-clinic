0c831762b93cb3d9b75ac181324f9aa88406affd
aditya109 <adikid1996@gmail.com>
2020-09-22 19:55:01 +0530

    implemented map based services; closes #6 Signed-off-by: aditya109 <adikid1996@gmail.com>
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ pet-clinic-data/src/main/java/io/github/aditya109/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package io.github.aditya109.sfgpetclinic.services.map;
+
+import io.github.aditya109.sfgpetclinic.model.Owner;
+import io.github.aditya109.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
